82179586b7cffee32b3b4bfa94c0065eede407f3
Sachinsree Babu <145961968+sachinsree47@users.noreply.github.com>
2025-06-11 04:26:07 +0000

    Updated crypto_utils.py

--- crypto_utils.py
+++ crypto_utils.py
@@ -5,10 +5,10 @@ from Crypto.Util.Padding import pad, unpad
 KEY = b'MyUltraSecretAES256KeyForTesting'
 
 def encrypt_file(data):
-    iv = get_random_bytes(16)  # 16 bytes IV for CBC
+    iv = get_random_bytes(16)  
     cipher = AES.new(KEY, AES.MODE_CBC, iv)
     encrypted_data = cipher.encrypt(pad(data, AES.block_size))
-    return iv + encrypted_data  # Store IV with encrypted data
+    return iv + encrypted_data  
 
 def decrypt_file(encrypted_data):
     iv = encrypted_data[:16]
